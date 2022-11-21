9897cdf27184e23f45c8b919150d0182b3292463
Ryan-Otoole <otoolerj@gmail.com>
2022-11-21 13:27:13 -0600

    recreated lesson14 backend ready to go

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { ethers } from 'ethers';
-import { AppService } from './app.service';
+import { AppService, createPaymentOrderDto, PaymentOrder, requestPaymentOrderDto } from './app.service';
 
 @Controller()
 export class AppController {
@@ -20,4 +20,28 @@ export class AppController {
    getTotalSupply(@Param('address') address: string): Promise<number> {
     return this.appService.getTotalSupply(address);
   }
+
+  @Get("allowance")
+    getAllowance(
+      @Query('address') address: string,
+      @Query('from') from: string,
+      @Query('to') to: string,
+    ): Promise<number> {
+      return this.appService.getAllowance(address, from, to);
+    }
+
+    @Get('payment-order/:id')
+      getPaymentOrder(@Param('id') id: number): any {
+        return this.appService.getPaymentOrder(id);
+      }
+
+    @Post('payment-order')
+      createPaymentOrder(@Body() body: createPaymentOrderDto): number {
+        return this.appService.createPaymentOrder(body.value, body.secret);
+      }
+
+      @Post('request-payment')
+        requestPaymentOrder(@Body() body: requestPaymentOrderDto): Promise<any> {
+        return this.appService.requestPaymentOrder(body.id, body.secret, body.receiver);
+        }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,11 +4,30 @@ import * as tokenJson from './assets/MyERC20Token.json';
 import * as dotenv from "dotenv";
 dotenv.config();
 
+export class PaymentOrder {
+  value: number;
+  id: number;
+  secret: string;
+}
+
+export class createPaymentOrderDto {
+  value: number;
+  secret: string;
+}
+
+export class requestPaymentOrderDto {
+  id: number;
+  secret: string;
+  receiver: string;
+}
+
 @Injectable()
 export class AppService {
 
   provider: ethers.providers.BaseProvider;
   erc20ContractFactory;
+  paymentOrders: PaymentOrder[];
+  signer;
 
   constructor() {
     this.provider = ethers.getDefaultProvider("goerli", {
@@ -18,8 +37,9 @@ export class AppService {
    });
 
    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
-   const signer = wallet.connect(this.provider);
-   this.erc20ContractFactory = new ethers.ContractFactory(tokenJson.abi, tokenJson.bytecode, signer);
+   this.signer = wallet.connect(this.provider);
+   this.erc20ContractFactory = new ethers.ContractFactory(tokenJson.abi, tokenJson.bytecode, this.signer);
+   this.paymentOrders = []
   }
 
   getBlock(blockHashOrTag: string = 'latest'): Promise<ethers.providers.Block> {
@@ -30,5 +50,33 @@ export class AppService {
     const contractInstance = this.erc20ContractFactory.attach(address);
     const totalSupply = await contractInstance.totalSupply();
     return parseFloat(ethers.utils.formatEther(totalSupply));
+  }
+
+  async getAllowance(address: string, from: string, to: string): Promise<number> {
+    const contractInstance = this.erc20ContractFactory.attach(address);
+    const allowance = await contractInstance.allowance(from, to);
+    return parseFloat(ethers.utils.formatEther(allowance));
+  }
+
+  getPaymentOrder(id: number) {
+    const paymentOrder = this.paymentOrders[id];
+    return { value: paymentOrder.value, id: paymentOrder.id };
+  }
+
+  createPaymentOrder(value:number, secret: string) {
+    const newPaymentOrder = new PaymentOrder();
+    newPaymentOrder.value = value;
+    newPaymentOrder.secret = secret;
+    newPaymentOrder.id = this.paymentOrders.length;
+    this.paymentOrders.push(newPaymentOrder);
+    return newPaymentOrder.id;
+  }
+
+  async requestPaymentOrder(id: number, secret: string, receiver: string) {
+    const paymentOrder = this.paymentOrders[id];
+    if (paymentOrder.secret != secret) throw new Error("WRONG SECRET");
+    const contractInstance = this.erc20ContractFactory.attach("0x0CBBA32981898231078CDAD4c621D734492CF02D");
+    const tx = await contractInstance.mint(receiver, paymentOrder.value);
+    return tx.wait();
   }
 }
